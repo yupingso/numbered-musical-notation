52f5c85544315c84594bce8e2773b427030af30b
yupingso <yupingso@gmail.com>
2017-10-10 17:11:30 +0800

    :construction: Test cv2.ellipse

--- test_pillow.py
+++ test_pillow.py
@@ -1,7 +1,10 @@
+import os
 from math import pi as PI, sin, cos, asin
 from pathlib import Path
 
+import numpy as np
 from PIL import Image, ImageFont, ImageDraw
+import cv2
 
 
 font_path = Path('latex/fonts').resolve()
@@ -12,8 +15,8 @@ enbi_font_file = font_path / 'timesbi.ttf'
 eni_font_file = font_path / 'timesi.ttf'
 
 
-if __name__ == '__main__':
-    image = Image.new('RGB', [1500, 1125], color=(0, 0, 100))
+def test_pillow():
+    image = Image.new('RGB', [1500, 1125], color=(0, 100, 100))
     draw = ImageDraw.Draw(image)
 
     # fonts
@@ -47,7 +50,7 @@ if __name__ == '__main__':
         draw.ellipse(((x - r, y - r), (x + r, y + r)), fill=(255, 255, 255))
 
     def draw_arc(draw, bbox, start, end, fill, width=1, segments=100):
-        """Hack that looks similar to PIL's draw.arc(), but can specify a line width."""
+        """Draw arc with line width specified."""
         if len(bbox) == 2:
             bbox = (bbox[0][0], bbox[0][1], bbox[1][0], bbox[1][1])
 
@@ -62,7 +65,7 @@ if __name__ == '__main__':
         start -= da / 2
         end -= da / 2
 
-        # ellips radii
+        # ellipse radii
         rx = (bbox[2] - bbox[0]) / 2
         ry = (bbox[3] - bbox[1]) / 2
 
@@ -76,7 +79,7 @@ if __name__ == '__main__':
         for i in range(segments):
 
             # angle centre
-            a = start + (i+0.5) * da
+            a = start + (i + 0.5) * da
 
             # x,y centre
             x = cx + cos(a) * rx
@@ -96,9 +99,29 @@ if __name__ == '__main__':
         angle = asin(d / r / 2) * (180 / PI)
         x_c = (x1 + x2) / 2
         y_c = y - h + r
-        # https://stackoverflow.com/questions/7070912/creating-an-arc-with-a-given-thickness-using-pils-imagedraw
         draw_arc(draw, ((x_c - r, y_c - r), (x_c + r, y_c + r)),
-                 270 - angle, 270 + angle, fill=(255, 255, 255), width=4, segments=1000)
+                 270 - angle, 270 + angle, fill=(255, 255, 255), width=4, segments=100)
+
+    def draw_tie_opencv(image, x1, x2, y, h, color=(255, 100, 255)):
+        # convert
+        img = np.array(np.asarray(image)[:, :, ::-1])
+
+        # draw tie
+        d = x2 - x1
+        r = (h * h + d * d / 4) / (h * 2)
+        assert r > 0
+        angle = asin(d / r / 2) * (180 / PI)
+        x_c = (x1 + x2) / 2
+        y_c = y - h + r
+        270 - angle, 270 + angle
+        cv2.ellipse(img, (int(x_c), int(y_c)), (int(r), int(r)), 0,
+                    270 - angle, 270 + angle, color, thickness=4)
+
+        # convert back
+        image = Image.fromarray(img[:, :, ::-1], 'RGB')
+        draw = ImageDraw.Draw(image)
+        return image, draw
+
 
     # melody
     y_m = 295
@@ -112,6 +135,8 @@ if __name__ == '__main__':
                (155+150 + size_m[0] / 2, y_m + size_m[1] / 2 + 20)), width=8)
     draw_circle(draw, 155, y_m + size_m[1] / 2 + 60, 8)
     draw_tie(draw, 155, 155 + 150, y_m - size_m[1] / 2, 30)
+    image, draw = draw_tie_opencv(image, 155 + 300, 155 + 450, y_m - size_m[1] / 2, 30)
+
 
     # lyrics
     draw_lyrics(draw, 155, 500, '每')
@@ -122,6 +147,40 @@ if __name__ == '__main__':
     # image.paste(sharp_im, mask=None)  # TODO
 
     # save to file
-    filename = Path.home() / 'htdocs' / 'test.png'
+    if os.name == 'nt':
+        filename = Path.home() / 'DeskTop' / 'test_pillow.png'
+    else:
+        filename = Path.home() / 'htdocs' / 'test_pillow.png'
     image.save(str(filename))
     print('Save to {}'.format(filename))
+
+
+def test_opencv():
+    img = np.zeros((1125, 1500, 3), np.uint8)
+    color = (0, 100, 100)
+    img[:] = tuple(reversed(color))
+
+    # text
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img, 'String!', (0, 100), font, 5, (255, 255, 255), 10, cv2.LINE_AA)
+
+    if os.name == 'nt':
+        filename = Path.home() / 'DeskTop' / 'test_opencv.png'
+    else:
+        filename = Path.home() / 'htdocs' / 'test_opencv.png'
+    cv2.imwrite(str(filename), img)
+    print('Save to {}'.format(filename))
+
+    image = Image.fromarray(img[:, :, ::-1], 'RGB')
+    image.save(filename.parent / 'test.png')
+
+
+if __name__ == '__main__':
+    test_pillow()
+    # test_opencv()
+
+    # PIL.Image image to ndarray img
+    # img = np.asarray(image)[:, :, ::-1]   # read-only
+
+    # ndarray img to PIL.Image image
+    # image = Image.fromarray(img[:, :, ::-1], 'RGB')
